fa4844f301c1b1e116eed168ef9918c12883cb05
ST10445089 <st10445089@vcconnect.edu.za>
2025-02-22 17:26:32 +0200

    Welcome method changed

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -11,10 +11,8 @@ public class HelloWorldController : Controller
     {
         return "This is my default action...";
     }
-    // 
-    // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
+    public string Welcome(string name, int ID = 1)
     {
-        return "This is the Welcome action method...";
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
     }
 }
